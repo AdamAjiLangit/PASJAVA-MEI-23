3766c86faac66744c9be1b4bd126bca38430acbc
Adam <adam.aji2007@gmail.com>
2023-05-26 23:03:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pasjava_mei_23/FilmAdapter.java
@@ -0,0 +1,14 @@
+package com.example.pasjava_mei_23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilmAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_adapter);
+    }
+}
